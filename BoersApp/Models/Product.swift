bc52a0efe6d86b5ab0163ffc9aa9ffd9a60b34d1
Dmitry Rozov <expertgames@yandex.ru>
2019-03-16 09:59:24 +0200

    Added Product model

---
+++ BoersApp/Models/Product.swift
@@ -0,0 +1,18 @@
+//
+//  Product.swift
+//  BoersApp
+//
+//  Created by Dmitry Rozov on 16/03/2019.
+//  Copyright © 2019 SPBSTU. All rights reserved.
+//
+
+import Foundation
+
+struct Product {
+
+    let id: Int
+    let name: String
+    let code: String
+
+    //TODO: - Add collection of steps
+}
