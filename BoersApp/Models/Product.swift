1b8736d4256419a2da78fe9e29b94b506d964aac
Alexandr <kostiukevich.2016@gmail.com>
2019-03-22 14:06:45 +0300

    Fixed swiftLint warnings

--- BoersApp/Models/Product.swift
+++ BoersApp/Models/Product.swift
@@ -9,10 +9,12 @@
 import Foundation
 
 struct Product {
-
+    //swiftlint:disable identifier_name
     let id: Int
+    //swiftlint:enable identifier_name
     let name: String
     let code: String
-
+    //swiftlint:disable todo
     //TODO: - Add collection of steps
+    //swiftlint:enable todo
 }
